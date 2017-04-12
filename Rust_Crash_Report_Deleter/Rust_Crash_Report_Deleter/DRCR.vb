631731d5f6f90399df314e30f326ce79e5a681ea
Bailey D <baileydunlap9@gmail.com>
2017-04-12 14:34:03 -0700

    Add files via upload

---
+++ Rust_Crash_Report_Deleter/Rust_Crash_Report_Deleter/DRCR.vb
@@ -0,0 +1,134 @@
+﻿Public Class DRCR
+    Private Sub DRCR_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i As Integer = 0
+
+        'Target Directory
+        Dim directory = "C:\Program Files (x86)\Steam\steamapps\common\Rust\"
+
+        'Searches directory and it's subdirectories for all files, which "*" stands for
+        'Say for example you only want to search for jpeg files... then change "*" to "*.jpg"  
+        For Each filename As String In IO.Directory.GetFiles(directory, "*", IO.SearchOption.AllDirectories)
+
+            'The next line of code gets only file extensions from searched directories and subdirectories
+            Dim fName As String = IO.Path.GetFileName(filename)
+
+            If fName = "_CommonRedist" Then
+
+                'Skips to next iteration of Loop, ignoring files with filename
+                Continue For
+
+            Else
+                If fName = "Bundles" Then
+
+                    'Skips to next iteration of Loop, ignoring files with filename
+                    Continue For
+
+                Else
+                    If fName = "cfg" Then
+
+                        'Skips to next iteration of Loop, ignoring files with filename 
+                        Continue For
+
+                    Else
+                        If fName = "EasyAntiCheat" Then
+
+                            'Skips to next iteration of Loop, ignoring files with filename
+                            Continue For
+
+                        Else
+                            If fName = "maps" Then
+
+                                'Skips to next iteration of Loop, ignoring files with filename
+                                Continue For
+
+                            Else
+                                If fName = "RustClient_Data" Then
+
+                                    'Skips to next iteration of Loop, ignoring files with filename
+                                    Continue For
+
+                                Else
+                                    If fName = "Screenshots" Then
+
+                                        'Skips to next iteration of Loop, ignoring files with filename
+                                        Continue For
+
+                                    Else
+                                        If fName = "storage" Then
+
+                                            'Skips to next iteration of Loop, ignoring files with filename
+                                            Continue For
+
+                                        Else
+                                            If fName = "EasyAntiCheat.Client.dll" Then
+
+                                                'Skips to next iteration of Loop, ignoring files with filename
+                                                Continue For
+
+                                            Else
+                                                If fName = "Rust.exe" Then
+
+                                                    'Skips to next iteration of Loop, ignoring files with filename 
+                                                    Continue For
+
+                                                Else
+                                                    If fName = "RustClient.exe" Then
+
+                                                        'Skips to next iteration of Loop, ignoring files with filename 
+                                                        Continue For
+
+                                                    Else
+                                                        If fName = "steam_api64.dll" Then
+
+                                                            'Skips to next iteration of Loop, ignoring files with filename
+                                                            Continue For
+
+                                                        Else
+                                                            If fName = "steam_appid.txt" Then
+
+                                                                'Skips to next iteration of Loop, ignoring files with filename
+                                                                Continue For
+
+                                                            Else
+                                                                If fName = "win_installscript.vdf" Then
+
+                                                                    'Skips to next iteration of Loop, ignoring files with filename
+                                                                    Continue For
+
+                                                                Else
+
+                                                                    'Kill function deletes all files in target directory and it's subdirectories 
+                                                                    System.IO.File.Delete(directory & fName)
+
+                                                                    'The below counter only displays the final count after all files have been processed
+                                                                    i = i + 1
+                                                                    TextBox1.Text = Convert.ToString(i)
+                                                                End If
+                                                            End If
+                                                        End If
+                                                    End If
+                                                End If
+                                            End If
+                                        End If
+                                    End If
+                                End If
+                            End If
+                        End If
+                    End If
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("IEXPLORE.exe", LinkLabel1.Text)
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+End Class
---
+++ Rust_Crash_Report_Deleter/Rust_Crash_Report_Deleter/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Rust_Crash_Report_Deleter")>
+<Assembly: AssemblyDescription("Deletes the crash report rust generates")>
+<Assembly: AssemblyCompany("https://forum.invictus-load.in/profile/3056-weeke/")>
+<Assembly: AssemblyProduct("Rust_Crash_Report_Deleter")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7b10312d-4ec3-4482-9f79-d98ed727c174")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
